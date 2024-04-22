cf0d46828bea474b5338c112f2f1304ffaf994d7
Destin Lee <dleeinc@gmail.com>
2024-04-22 11:27:11 -0500

    2.done text node convertion

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode
+from textnode import TextNode, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -9,6 +9,48 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", "bold")
 
         self.assertEqual(node, node2)
+
+    def test_text_node_to_html_node_text(self):
+        node = TextNode("im text")
+        converted_text_node = text_node_to_html_node(node)
+        test_str = f"im text"
+
+        self.assertEqual(converted_text_node.to_html(), test_str)
+
+    def test_text_node_to_html_node_bold(self):
+        node = TextNode("im bold", "bold")
+        converted_text_node = text_node_to_html_node(node)
+        test_str = f"<b>im bold</b>"
+
+        self.assertEqual(converted_text_node.to_html(), test_str)
+
+    def test_text_node_to_html_node_italic(self):
+        node = TextNode("im italic", "italic")
+        converted_text_node = text_node_to_html_node(node)
+        test_str = f"<i>im italic</i>"
+
+        self.assertEqual(converted_text_node.to_html(), test_str)
+
+    def test_text_node_to_html_node_code(self):
+        node = TextNode("im code", "code")
+        converted_text_node = text_node_to_html_node(node)
+        test_str = f"<code>im code</code>"
+
+        self.assertEqual(converted_text_node.to_html(), test_str)
+
+    def test_text_node_to_html_node_link(self):
+        node = TextNode("destinio", "link", "https://destin.io")
+        converted_text_node = text_node_to_html_node(node)
+        test_str = f'<a href="https://destin.io">destinio</a>'
+
+        self.assertEqual(converted_text_node.to_html(), test_str)
+
+    def test_text_node_to_html_node_image(self):
+        node = TextNode("destinio", "image", "https://destin.io")
+        converted_text_node = text_node_to_html_node(node)
+        test_str = f'<img src="https://destin.io" alt="destinio">destinio</img>'
+
+        self.assertEqual(converted_text_node.to_html(), test_str)
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,16 @@
+from htmlnode import LeafNode
+
+
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
 class TextNode:
-    def __init__(self, text, text_type, url=None):
+    def __init__(self, text, text_type=text_type_text, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
@@ -15,3 +26,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    if text_node.text_type == text_type_text:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == text_type_bold:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == text_type_italic:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == text_type_code:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == text_type_link:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == text_type_image:
+        return LeafNode(
+            "img", text_node.text, {"src": text_node.url, "alt": text_node.text}
+        )
+
+    raise ValueError(f"text_type is wrong: {text_node.text_type}")
